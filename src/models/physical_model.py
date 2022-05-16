2660f24a107b9c90874e295aceec97a2b4eb42f4
simonmoesorensen <moe.simon@gmail.com>
2022-05-16 10:48:00 +0200

    Refactored PhysicalModel to sklearn interface

--- src/models/physical_model.py
+++ src/models/physical_model.py
@@ -1,26 +1,51 @@
 from numpy import sin, sign, cos
 
 # Constants
-Ci0 = 0.05  # Rolling inertia coefficient
-Cd = 0.29  # Air drag coefficient
-rho = 1.225  # Density of air
-g = 9.81  # Gravitational acceleration
-Vwind = 0  # Velocity of wind
-A = 2.33  # Cross-sectional area of car
-mcar = 1966  # Mass of car
-mp = 80  # Mass of passengers
-theta = 0  # Road slope
+from sklearn.base import BaseEstimator, RegressorMixin
 
 
-def get_traction_power(V, ux):  # Velocity, x-axis acceleration
-    Crr = 0.01 * (1 + V) / 100  # Rolling resistance coefficient
+class PhysicalModel(BaseEstimator, RegressorMixin):
 
-    Fhc = (mcar + mp) * g * sin(theta)  # Hill climbing force
-    Faero = sign(V + Vwind) * 1/2 * rho * A * Cd * (V + Vwind)**2  # Aerodynamic drag
-    Fi = Ci0 * (mcar + mp) * ux  # Inertial force
-    Frr = sign(V) * (mcar + mp) * g * cos(theta) * Crr  # Rolling resistance force of the wheels
+    def __init__(self,
+                 Ci0=0.05,   # Rolling inertia coefficient
+                 Cd=0.29,    # Air drag coefficient
+                 rho=1.225,  # Density of air
+                 g=9.81,     # Gravitational acceleration
+                 Vwind=0,    # Velocity of wind
+                 A=2.33,     # Cross-sectional area of car
+                 mcar=1966,  # Mass of car
+                 mp=80,      # Mass of passengers
+                 theta=0):   # Road slope
 
-    Ft = Fhc + Faero + Fi + Frr
+        self.theta = theta
+        self.mp = mp
+        self.mcar = mcar
+        self.A = A
+        self.Vwind = Vwind
+        self.g = g
+        self.rho = rho
+        self.Cd = Cd
+        self.Ci0 = Ci0
 
-    Pt = Ft * V
-    return Pt
+    def fit(self, V_col, ux_col):
+        self.V = V_col
+        self.ux = ux_col
+
+    def predict(self, X):
+        if self.V is None or self.ux is None:
+            raise ValueError('Please run .fit')
+
+        V = X[self.V]
+        ux = X[self.ux]
+
+        Crr = 0.01 * (1 + V) / 100  # Rolling resistance coefficient
+
+        Fhc = (self.mcar + self.mp) * self.g * sin(self.theta)  # Hill climbing force
+        Faero = sign(V + self.Vwind) * 1 / 2 * self.rho * self.A * self.Cd * (V + self.Vwind) ** 2  # Aerodynamic drag
+        Fi = self.Ci0 * (self.mcar + self.mp) * ux  # Inertial force
+        Frr = sign(V) * (self.mcar + self.mp) * self.g * cos(self.theta) * Crr  # Rolling resistance force of the wheels
+
+        Ft = Fhc + Faero + Fi + Frr
+
+        Pt = Ft * V
+        return Pt
